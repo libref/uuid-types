497d720981fe8338db957fc347bd28f6de3c4221
Bas Hamar de la Brethonière <bas@basje.com>
2018-01-03 22:19:21 +0100

    Add tests

---
+++ tests/unit/TestCase.php
@@ -0,0 +1,37 @@
+<?php
+namespace LibRef\Uuid\Tests;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+abstract class TestCase extends PHPUnitTestCase
+{
+    protected function assertException($test, $expectedException = 'Exception')
+    {
+        try {
+            $test();
+        } catch (Exception $e) {
+            $this->assertSame($expectedException, get_class($e));
+            return;
+        }
+
+        $this->assertSame($expectedException, 'No exception thrown');
+    }
+
+    protected function assertExceptionInstance($test, $expectedException = 'Exception')
+    {
+        try {
+            $test();
+        } catch (Exception $e) {
+            $this->assertInstanceOf($expectedException, get_class($e));
+            return;
+        }
+
+        $this->assertSame($expectedException, 'No exception thrown');
+    }
+
+    protected function doesTestSetContainsInstancesOf(array $testSet, $class)
+    {
+        foreach ($testSet as $test) {
+            $this->assertInstanceOf($class, $test);
+        }
+    }
+}
---
+++ tests/unit/ValueObjects/Uuid4Test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LibRef\Uuid\Tests\ValueObjects;
+
+
+use LibRef\Uuid\Tests\TestCase;
+use LibRef\Uuid\Uuid;
+use LibRef\Uuid\Uuid4;
+
+class Uuid4Test extends TestCase
+{
+    public function testExistence()
+    {
+        $this->assertTrue(class_exists(Uuid::class));
+        $this->assertTrue(class_exists(Uuid4::class));
+    }
+}
